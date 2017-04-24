101d1b3769225d29a8d0669196904f2c8ca4f829
dhcodes <thor.dh@gmail.com>
2017-04-24 15:59:47 -0500

    progress?

--- coord-update.js
+++ coord-update.js
@@ -1,29 +1,12 @@
 const fs = require('fs');
-//let data = require('./assets/json/campsites.json');
+let data = require('./assets/json/campsites.json');
 const NodeGeocoder = require('node-geocoder');
-let data = [ 
-{ 
-    url: 'https://www.facebook.com/groups/free.code.camp.herat',
-    city: 'Herat',
-    state: '',
-    country: 'Afghanistan',
-    coordinates: '34.3529° N, 62.2040° E',
-    photoUrl: 'https://scontent-dft4-2.xx.fbcdn.net/v/t1.0-0/p200x200/16142712_1380709768660732_3807276804030541539_n.jpg?oh=c25edd0bc923c2018e56c2da69073c9d&oe=59942E99' },
-{ 
-    url: 'https://www.facebook.com/groups/free.code.camp.kabul',
-    city: 'Kabul',
-    state: '',
-    country: 'Afghanistan',
-    coordinates: '34.5553° N, 69.2075° E',
-    photoUrl: 'https://scontent-dft4-2.xx.fbcdn.net/v/t31.0-8/1909236_804880492967338_4532988271657391692_o.jpg?oh=abf0b929866e2640d518e886759d094d&oe=5958C9E2' 
-}
-]
 let options = {
   provider: 'google',
  
   // Optional depending on the providers 
   httpAdapter: 'https', // Default 
-  apiKey: '', // for Mapquest, OpenCage, Google Premier 
+  apiKey: 'AIzaSyAkLMmqiXiVLr2bpo3jOHLY4ITbH6ox9zo', // for Mapquest, OpenCage, Google Premier 
   formatter: null         // 'gpx', 'string', ... 
 };
  
@@ -31,7 +14,7 @@ let geocoder = NodeGeocoder(options);
 
 
 let json = data.map((obj) =>  {
-        geocoder.geocode(obj.city + ', ' + obj.state + ', ' + obj.country, (err, res) => {
+        return geocoder.geocode(obj.city + ', ' + obj.state + ', ' + obj.country, (err, res) => {
             if (err) {
         console.log(err)
     }
@@ -51,4 +34,4 @@ console.log(json)
 
 
 
-//fs.writeFile('./assets/json/campsiteclone.json', json)
+fs.writeFile('./assets/json/campsiteclone.json', json)
